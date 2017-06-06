a54d2da9f707b31441e332541bd2f32a98eb5365
Geoffroy Lesage <gefthefrench@gmail.com>
2017-06-06 20:00:53 +0200

    Added deletion handling

--- cache.js
+++ cache.js
@@ -9,8 +9,11 @@ module.exports = function (app)
         if (!req.body) errorMsg = 'body is required';
         else if (!req.body.modelName) errorMsg = 'modelName is required';
         else if (!req.body.methodName) errorMsg = 'methodName is required';
-        else if (!req.body.data) errorMsg = 'data is required';
-        else if (!req.body.data.id) errorMsg = 'model id is required in data';
+        else if (req.body.methodName === 'create' && !req.body.data) errorMsg = 'data is required for create';
+        else if (req.body.methodName === 'update' && !req.body.data) errorMsg = 'data is required for update';
+        else if (req.body.methodName === 'create' && !req.body.data.id) errorMsg = 'model id is required for create';
+        else if (req.body.methodName === 'update' && !req.body.data.id) errorMsg = 'model id is required for update';
+        else if (req.body.methodName === 'delete' && !req.body.modelId) errorMsg = 'modelId is required for deletion';
 
         if (errorMsg) res.status(400).send(errorMsg);
         else
@@ -20,7 +23,10 @@ module.exports = function (app)
             // If there is not even an empty dictionary for this modelName
             // if means this cache is not listening for the model, so only
             // add the data if we actually care about it
-            if (localData) localData[req.body.data.id] = req.body.data;
+            if (req.body.data && localData) localData[req.body.data.id] = req.body.data;
+
+            // If there is no data, it means it's a deletion
+            else if (req.body.modelId && req.body.methodName === 'delete') delete localData[req.body.modelId];
 
             res.status(200).send();
         }
